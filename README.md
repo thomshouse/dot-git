e116f62afdf88ce422ab1ab7c957061b281789d7
thomshouse@gmail.com <thomshouse@gmail.com>
2022-04-05 22:23:32 -0700

    Refactor command verbalization

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # thomshouse/dot-git
+
 (Opinionated) dotfiles package for git by [thomshouse][thomshouse].
+
+This package contains various aliases and completion settings, and also wraps the main `git` command in a function that provides some added utility.
+
+Additionally, the git and system aliases in this package are configured to display the long-form version of the aliased commands on execution.  If you wish to suppress these messages, you can create an empty file named `.dotfiles_hide_git_verbalizing` in your home folder.
 
 ## Install
 Clone and symlink or install with [ellipsis][ellipsis]:
--- init/git.zsh
+++ init/git.zsh
@@ -16,31 +16,33 @@ git() {
 	    else
 	      ALIASOF="git $ALIASOF";
 	    fi;
-	    dotfiles_git_verbalize "$ALIASOF$ALIASARGS";
+	    _dotfiles_git_verbalize "$ALIASOF$ALIASARGS";
     fi
     command git "$@"
   fi
 }
 
-dotfiles_git_verbalize() {
-  echo "  \$ \e[1m\e[97m$@\e[0m";
+_dotfiles_git_verbalize() {
+  if [ ! -f "$HOME/.dotfiles_hide_git_verbalizing" ]; then
+    echo "  \$ \e[1m\e[97m$@\e[0m";
+  fi
 }
 
 # Aliases to git commands
 alias g="git"
-alias ga="dotfiles_git_verbalize git add && git add"
-alias gb="dotfiles_git_verbalize git branch && git branch"
-alias gcb="dotfiles_git_verbalize git checkout -b && git checkout -b"
-alias gch="dotfiles_git_verbalize git checkout && git checkout"
-alias gcm="dotfiles_git_verbalize git commit && git commit"
-alias gd="dotfiles_git_verbalize git diff && git diff"
-alias gf="dotfiles_git_verbalize git fetch && git fetch"
-alias gff="dotfiles_git_verbalize git merge --ff-only && git merge --ff-only"
-alias gm="dotfiles_git_verbalize git merge && git merge"
-alias gp="dotfiles_git_verbalize git push && git push"
-alias gr="dotfiles_git_verbalize git remote && git remote"
-alias gs="dotfiles_git_verbalize git status && git status"
-alias gup="dotfiles_git_verbalize git remote update -p && git remote update -p; dotfiles_git_verbalize git merge --ff-only @{u} && git merge --ff-only @{u}"
+alias ga="_dotfiles_git_verbalize git add && git add"
+alias gb="_dotfiles_git_verbalize git branch && git branch"
+alias gcb="_dotfiles_git_verbalize git checkout -b && git checkout -b"
+alias gch="_dotfiles_git_verbalize git checkout && git checkout"
+alias gcm="_dotfiles_git_verbalize git commit && git commit"
+alias gd="_dotfiles_git_verbalize git diff && git diff"
+alias gf="_dotfiles_git_verbalize git fetch && git fetch"
+alias gff="_dotfiles_git_verbalize git merge --ff-only && git merge --ff-only"
+alias gm="_dotfiles_git_verbalize git merge && git merge"
+alias gp="_dotfiles_git_verbalize git push && git push"
+alias gr="_dotfiles_git_verbalize git remote && git remote"
+alias gs="_dotfiles_git_verbalize git status && git status"
+alias gup="_dotfiles_git_verbalize git remote update -p && git remote update -p; _dotfiles_git_verbalize git merge --ff-only @{u} && git merge --ff-only @{u}"
 
 # Aliases to git aliases -- due to structure of commands
 alias gffon="git ffon"
